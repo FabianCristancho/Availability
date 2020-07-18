ba5f81dd7a1f4cde8423e0c99e4e43880752feee
FabianCristancho <fabiancrisrincon@gmail.com>
2020-07-17 22:51:52 -0500

    add scripts

---
+++ remote.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#sudo apt-get install sshpass
+#https://www.franciscougalde.com/2018/03/11/sshpass-conectate-via-ssh-a-tu-servidor-con-una-sola-linea-de-comando/
+#https://www.it-swarm.dev/es/bash/como-ssh-desde-dentro-de-un-script-bash/968581533/
+echo "conectando a servidor de forma remota..."
+sleep 2
+echo "bajando ultima actualizacion del proyecto..."
+sshpass -p '1234567' ssh -o StrictHostKeyChecking=no server1@192.168.100.136 'cd lab1/Availability;git pull'
+
+
+
+
+
---
+++ time_total.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+time_response=$(curl -o /dev/null -L -qs -w "%{time_total}" www.uptc.edu.co)
+time_required=1.4
+echo $time_required
+echo $time_response
+
+if awk 'BEGIN{exit ARGV[1]>ARGV[2]}' "$time_response" "$time_required"
+then
+     echo "el tiempo de respuesta es menor que el solicitado"
+else
+     echo "el tiempo de respuesta es mayor que el solicitado"
+     echo "conectando a servidor de forma remota..."
+     sleep 2
+     echo "bajando ultima actualizacion del proyecto..."
+     sshpass -p '1234567' ssh -o StrictHostKeyChecking=no server1@192.168.100.136 'cd lab1/Availability;git pull'
+fi
+
+
+
+
+
